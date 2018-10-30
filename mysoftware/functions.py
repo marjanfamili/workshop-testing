2a7463314751fc804cc55b240cb273b2f448d9c0
marjanfamili <m.famili@soton.ac.uk>
2018-10-30 17:12:03 +0000

    lets commit

---
+++ mysoftware/functions.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+def square(x):
+	'''
+	Returns the square of the input x
+
+	Longer description - the square is found by multiplying x by x...
+
+	Parameters:
+	-----------
+	x, float:
+	   Base number
+
+	Returns:
+	--------
+	float:
+	   Base number raised to power 2
+
+	See Also:
+	---------
+	pow : raise an argument to an arbitrary power
+
+	Examples
+	--------
+
+	>>> squared(2)
+	4
+
+	>>> squared(8)
+	64
+
+	>>> squared(-1)
+	1
+	'''
+	return x*x
---
+++ test_mysoftware.py
@@ -0,0 +1,17 @@
+from mysoftware import *
+
+def test_square_integers():
+        assert square(2) == 4
+        assert square(0) == 0
+        assert square(-1) == 1
+
+
+
+'''
+def test_coloumb_throws_error():
+    try:
+        coloumb(0.0)
+        raise Exception("test failed")
+    except ValueError:
+        pass
+'''
